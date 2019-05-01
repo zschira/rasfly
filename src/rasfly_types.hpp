8f8eaac202dcc2c98ca056aa833731654f86ad2f
zschira <zachary.w.schira@nasa.gov>
2019-05-01 14:16:07 -0400

    Add State constructor from json object

--- src/rasfly_types.hpp
+++ src/rasfly_types.hpp
@@ -1,6 +1,8 @@
 #ifndef _RASFLY_TYPES_H_
 #define _RASFLY_TYPES_H_
 
+#include "json.hpp"
+
 struct Quaternion {
 	double w;
 	double x;
@@ -9,6 +11,16 @@ struct Quaternion {
 };
 
 struct State {
+	State() {}
+	State(nlohmann::json json_str) {
+		x = json_str["x"];
+		y = json_str["y"];
+		z = json_str["z"];
+		quaternion.w = json_str["qw"];
+		quaternion.x = json_str["qx"];
+		quaternion.y = json_str["qy"];
+		quaternion.z = json_str["qz"];
+	}
 	// Position
 	double x;
 	double y;
